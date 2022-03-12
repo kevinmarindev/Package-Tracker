778b6a9e7c1f114436ee3fe251f9284e7db395bc
kevinmarindev <kevin.marin7@hotmail.com>
2022-03-12 01:37:15 -0600

    login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Login
