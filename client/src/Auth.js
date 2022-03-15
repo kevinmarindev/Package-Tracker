062895c5d1178b1e369990c4b525ffea07e8bb37
kevinmarindev <kevin.marin7@hotmail.com>
2022-03-14 19:17:55 -0500

    auth component

---
+++ client/src/Auth.js
@@ -0,0 +1,15 @@
+// import { useEffect } from 'react'
+// import axios from 'axios'
+// import { useState, useEffect } from 'react'
+// import { useNavigate } from 'react-router-dom'
+
+const Auth = ({ children, sendTo }) => {
+    
+    // return (
+    //     <div>
+            
+    //     </div>
+    // )
+}
+
+export default Auth
