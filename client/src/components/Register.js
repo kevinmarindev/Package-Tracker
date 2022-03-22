2b735e8bc698fc05dbbf482f993f195621e7df57
kevinmarindev <kevin.marin7@hotmail.com>
2022-03-21 19:23:52 -0500

    registr componet

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,17 +1,26 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Alert from 'react-bootstrap/Alert';
 import axios from 'axios';
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
-const Register = () => {
+const Register = ({ pullUserToAppCompo }) => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [errs, setErrors] = useState('')
+    // const [user, setUser] = useState('')
+
+    const navigate = useNavigate()
+
+    let user
 
     const registering = async (e) => {
+        console.log(username, email, password, confirmPassword)
         e.preventDefault()
         let res = await axios.post('/register', {
             username: username,
@@ -19,24 +28,41 @@ const Register = () => {
             password: password,
             confirmPassword: confirmPassword
         },{withCredentials: true})
-        // let res = await fetch('/register', {
-        //         method: 'POST',
-        //         headers: {'Content-Type': 'application/json'},
-        //         body: JSON.stringify({
-        //             "username": username,
-        //             "email": email,
-        //             "password": password,
-        //             "confirmPassword": confirmPassword
-        //       })
-        //     })
+        
+        console.log(res, res.data)
 
-            
-        // console.log(res, res.session, res.user)
-        console.log(res, res.data, res.data.errors, res.session, res.user)
+        if(res.data.errors){
+            setErrors((prev) => res.data.errors)
+            console.log(errs)
+        } 
+        
+        if(res.data.user){
+            // setUser((prev)=> res.data.user)
+            user = res.data.user
+            console.log(user)
+            pullUserToAppCompo(user)
+            // navigate('/in')
+        }
 
+        // if(user){
+        //     console.log(user)
+
+        // }
+        // if(res.data.user){
+        //     console.log(user)
+        //     pullUser(res.data.user)
+        //     navigate('/in')
+        // }
+    // }
+    // if(user){
+    //     console.log(user)
+    //     pullUser(user)
+    //     navigate('/in')
     }
+    console.log('2nd', user)
   return (
     <Container>
+      {errs ? errs.map((err, idx) => <Alert variant="danger" key={idx}><Alert.Heading>{err}</Alert.Heading></Alert>) : ''}  
       <Form className='mt-5 mx-5'>
         <Form.Group className="mb-3" controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
@@ -71,3 +97,17 @@ const Register = () => {
 };
 
 export default Register;
+
+// let res = await fetch('/register', {
+        //         method: 'POST',
+        //         headers: {'Content-Type': 'application/json'},
+        //         body: JSON.stringify({
+        //             "username": username,
+        //             "email": email,
+        //             "password": password,
+        //             "confirmPassword": confirmPassword
+        //       })
+        //     })
+
+            
+        // console.log(res, res.session, res.user)
