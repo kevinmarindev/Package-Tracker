7adfa73fd20340d4f4a575db921ad8388bd36124
kevinmarindev <kevin.marin7@hotmail.com>
2022-03-21 21:58:34 -0500

    one one

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,24 @@ import Auth from './Auth'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // import { useEffect } from 'react'
 // import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
+//testing this bee
+//other test
 
 function App() {
-  
+  console.log('app rerender')
+  // const [user, setUser] = useState('')
   //------------------------------END STATE---------------------------------------//
-
+  const userLoggedIn = useRef('')
+  
   const pullUserToAppCompo = (dataIn) => {
     console.log(dataIn)
-    setUser(true)
+    userLoggedIn.current = dataIn
+    console.log(userLoggedIn)
   }
-
-  console.log(user)
+  console.log(userLoggedIn)
+  // console.log(user)
 
   return (
     <Router>
@@ -31,13 +36,13 @@ function App() {
           element={<Login />}
         />
         <Route path='/register'
-          element={<Register pullUserToAppCompo={pullUserToAppCompo}></Register>}
+          element={<Register pullUserToAppCompo={pullUserToAppCompo} userLoggedIn={userLoggedIn}></Register>}
         />
         <Route path='/in'
           element={
-            <Auth sendTo='/in'>
+            
               <Display />
-            </Auth>
+            
           }
         />
         <Route path='*' element={<h1>Hey there you need to be logged in to see this</h1>}></Route>
