ce24aee61179f0a468584056ef2eb9905c8a858e
kevinmarindev <kevin.marin7@hotmail.com>
2022-04-19 00:56:03 -0500

    changes to ups route

--- routes/numsRoutes.js
+++ routes/numsRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const TrackSchema = require('../models/Numbers')
 const axios = require('axios')
 const md5 = require('md5')
+const translate = require('translate')
+
 
 
 router.post('/posttracking', async (req, res)=>{
@@ -70,8 +72,9 @@ router.get('/gettrackings', async (req, res)=>{
     res.send(numbers)
 })
 
-router.post('/getsingletracking', async (req, res)=>{
-    // console.log(req.user.username)
+
+router.post('/getsingletrackingups', async (req, res)=>{
+    console.log(req.user.username)
     let carrier = req.body.carrier
     let trackingNumber = req.body.tracknum
     // let description = req.body.description
@@ -98,11 +101,67 @@ router.post('/getsingletracking', async (req, res)=>{
         }
         })
         let obj = apiFetch.data
-        console.log(obj)
+        console.log(obj.data.carrier_id)
 
+        let arr = obj.data.items
 
+        arr.map( async (item, idx)=>{
+            item.context = item.context.replace(/.*\]/, '')
+            console.log(item.context)
+            const trans = await translate(item.context, { to: 'English', from: 'Chinese' })
+            console.log(trans)
+            item.context = trans
+            console.log(obj.data.items[idx].context)
+            obj.data.items[idx].context = trans
+            
+            console.log(obj.data.items[idx].context)
+            console.log(obj.data.items[idx].context)
+            if(idx == arr.length - 1) res.send(obj.data)
+        })
+
+    }
+)
+
+router.post('/getsingletracking', async (req, res)=>{
+    console.log(req.user.username)
+    let carrier = req.body.carrier
+    let trackingNumber = req.body.tracknum
+    // let description = req.body.description
+    console.log(carrier, trackingNumber)
+    const encrypKey = () => {
+        const obj2Encrypt = `{"area_show":1,"carrier_id":"${carrier}","order":"desc","phone":"","ship_from":"","ship_to":"","tracking_number":"${trackingNumber}"}sovUoovfRIZI7116b865c79955714c2b86e50213afc3f84e`;
+        return md5(obj2Encrypt).toUpperCase()
+    }
+
+    let signature = encrypKey() 
+
+    let apiFetch = await axios.post('https://www.kd100.com/api/v1/tracking/realtime', {
+           area_show: 1,
+           carrier_id: carrier,
+           order: "desc",
+           phone: "",
+           ship_from: "",
+           ship_to: "",
+           tracking_number: trackingNumber
+        }, {withCredentials: true, headers: {
+            "Content-Type": "application/json",
+            "API-Key": "sovUoovfRIZI7116",
+            "signature": signature
+        }
+        })
+        let obj = apiFetch.data
+        console.log(obj.data.carrier_id)
+
+        let arr = obj.data.items
+
+        arr.map(item =>{
+            item.context = item.context.replace(/.*\]/, '')
+        })
+
+        
         res.send(obj.data)
-})
+    }
+)
 
 
 router.delete('/deleteone', async (req, res)=>{
